fdc6dca60ddec074343d8b611414ca4dea38defe
Sachin Naik <srnaik@hotmail.com>
2016-11-16 20:27:47 -0800

    Hotfix completed

--- app/src/main/java/com/example/sac/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/sac/myfirstapp/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.sac.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.firstActivity);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                final Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
